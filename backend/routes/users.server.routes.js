1ace5c44f2469084bda59a26b4d05b8864687637
IsaacRid <isaac.rideal@gmail.com>
2026-02-16 00:20:11 +0000

    fix: Removed app from beggining of HTTP methods

--- backend/routes/users.server.routes.js
+++ backend/routes/users.server.routes.js
@@ -2,11 +2,11 @@ const users = require(`./controllers/users.server.controllers`)
 
 module.exports = function (app) {
     app.route("/api/users/register")
-    app.post(users.register)
+        .post(users.register)
     app.route("/api/users/login")
-    app.post(users.login)
+        .post(users.login)
     app.route("/api/users/logout")
-    app.post(users.logout)
+        .post(users.logout)
     app.route("/api/users/me")
-    app.get(users.getProfile)
+        .get(users.getProfile)
 }
